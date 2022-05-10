e93b6af9148ff13a4aaa73fd8508e1de8651c3ab
Kazalisto <keath.kaza@gmail.com>
2022-05-11 00:43:32 +0200

    final-feedback-app

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,16 +2,19 @@ import FeedbackItems from './FeedbackItems'
 import { useContext } from 'react'
 import {motion, AnimatePresence} from 'framer-motion'
 import FeedbackContext from '../context/FeedbackContext'
+import Spinner from './shared/Spinner'
+
 
 function FeedbackList() {
-    const {feedback} = useContext(FeedbackContext)
+    const {feedback, isLoading} = useContext(FeedbackContext)
 
-    if(!feedback || feedback.length === 0) {
+    if(!isLoading &&(!feedback || feedback.length === 0)) {
        return <p>No feedback yet</p>
     }
-return (
-    
-    <div className='feedback-list'>
+
+    return isLoading ? (
+    <Spinner /> ): (   
+       <div className='feedback-list'>
         <AnimatePresence>
         {feedback.map((item)=>(
             <motion.div 
@@ -26,7 +29,9 @@ return (
         ))}
         </AnimatePresence>
     </div>
-  )   
+
+    )
+ 
 
 
 
